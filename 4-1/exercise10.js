f798696b88526f655460257a8e4d74e090f69457
MCoutinho <matheuscavalcante88@hotmail.com>
2020-07-13 19:29:35 -0300

    alter ex 10 and 11

--- 4-1/exercise10.js
+++ 4-1/exercise10.js
@@ -2,9 +2,9 @@ let costPrice = 9;
 
 let sellPrice = 16;
 
-if (costPrice != 0 | sellPrice != 0) {
-  let profit = (sellPrice - costPrice) * 1000 * 0.2;
-  console.log(profit);
+if (costPrice > 0 && sellPrice > 0) {
+  let profit = (sellPrice - costPrice) * 1000 * 0.8;
+  console.log("Lucro:" + profit);
 } else {
   console.log("Erro, valores incorretos");
 }
