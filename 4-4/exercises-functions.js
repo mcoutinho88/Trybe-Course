071baf5baff2fd7e38c7cd8ae9d0c1bd26342008
MCoutinho <matheuscavalcante88@hotmail.com>
2020-07-16 18:00:55 -0300

    4-4: exercises-functions 5, 6

--- 4-4/exercises-functions.js
+++ 4-4/exercises-functions.js
@@ -32,9 +32,43 @@ function longestString(arr) {
   return arr[indexName];
 }
 
+function mostOcorrence(arr) {
+  let countNumber = 0;
+  let countRepeated = 0;
+  let indexNumber;
+  
+  for(let index_of_number in arr) {
+    let number = arr[index_of_number];
+    for (let comparing_index in arr) {
+      if (number === arr[comparing_index]) {
+        countNumber += 1;
+      }
+    }
+    if (countNumber > countRepeated) {
+      countRepeated = countNumber;
+      indexNumber = index_of_number
+    }
+    countNumber = 0;
+  }
+
+  return arr[indexNumber];
+
+}
+
+function sumArray(number) {
+  let sum = 0;
+  
+  for (let i = 1; i <= number; i += 1) {
+    sum += i
+  }
+  return sum;
+}
+
 let array = [2, 3, 6, 7, 10, 1];
 let array2 = [2, 4, 6, 7, 10, 0, -3];
 let array3 = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+let array4 = [2, 3, 2, 5, 8, 2, 3];
+let N = 6;
 
 console.log("Exercicio 1:")
 console.log(verificaPalindrome('arara'));
@@ -49,3 +83,8 @@ console.log(array2 + " -> index of lowest value: " + indexOfLowestValue(array2))
 console.log("Exercicio 4:")
 console.log(array3 + " -> longest name: " + longestString(array3));
 
+console.log("Exercicio 5:")
+console.log(array4 + " -> most ocorrance: " + mostOcorrence(array4));
+
+console.log("Exercicio 6:")
+console.log("N = " + N + " -> most ocorrance: " + sumArray(N));
