5381a71384249c9b8402c0ef840771a8906c2f6c
MCoutinho <matheuscavalcante88@hotmail.com>
2020-07-16 15:47:39 -0300

    exercises 4, 5

--- 4-4/exercises.js
+++ 4-4/exercises.js
@@ -13,8 +13,29 @@ console.log("Exercicio 2:");
 console.log(info);
 
 
-console.log("Exercicio 3: \n");
+console.log("\nExercicio 3: \n");
 
 for (let i in info) {
   console.log(i);
 }
+
+console.log("\nExercicio 4: \n");
+
+for (let i in info) {
+  console.log(info[i]);
+}
+
+
+console.log("\nExercicio 5: \n");
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: 'Sim'
+};
+
+console.log(info.personagem + " e " + info2.personagem);
+console.log(info.origem + " e " + info2.origem);
+console.log(info.nota + " e " + info2.nota);
+console.log("Ambos recorrentes");
