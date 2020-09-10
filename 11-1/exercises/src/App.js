00ee27cc6ef632b0a9976f8fed5cd6c1befc3a3c
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-10 18:18:56 -0300

    minor changes

--- 11-1/exercises/src/App.js
+++ 11-1/exercises/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const task = (value) => {
-  return (
-    <li>{value}</li>
-  );
-}
-
 function App() {
+
+  const task = (value) => {
+    return (
+      <li>{value}</li>
+    );
+  };
+
   const tasks = ['Estudar React', 'Estudar Javascript', 'Estudar CSS'];
   return (
     <div className="App">
